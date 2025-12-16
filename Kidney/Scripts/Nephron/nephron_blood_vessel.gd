b392e402799398edfbcc330f8cfc149c117aa3dc
Bigfeta1 <bigfetabeats@gmail.com>
2025-12-16 13:59:36 -0500

    updated PCT display

--- Kidney/Scripts/Nephron/nephron_blood_vessel.gd
+++ Kidney/Scripts/Nephron/nephron_blood_vessel.gd
@@ -4,9 +4,7 @@ signal concentrations_updated
 
 @onready var electrochemical_field = $ElectrochemicalField
 
-var compartment: PCTBloodCompartment
-
-# Compartment variables (delegated to compartment instance)
+# Compartment ion/solute counts
 var sodium: int = 0
 var potassium: int = 0
 var chloride: int = 0
@@ -27,36 +25,60 @@ var actual_protons: float = 0
 
 var volume: float = 5e-12
 
+@export var debug_mode: bool = false:
+	set(value):
+		debug_mode = value
+		if is_node_ready():
+			_initialize_concentrations()
+
+@export var debug_scale_factor: float = 1.32e-5
+
 func _ready():
-	# Create compartment instance
-	compartment = PCTBloodCompartment.new()
-	add_child(compartment)
-
-	# Connect compartment signal to relay it
-	compartment.concentrations_updated.connect(_on_compartment_updated)
-
 	# Set electrochemical field volume
 	if electrochemical_field:
 		electrochemical_field.volume = volume
 
-func _on_compartment_updated():
-	# Copy values from compartment for display access
-	sodium = compartment.sodium
-	potassium = compartment.potassium
-	chloride = compartment.chloride
-	amino_acids = compartment.amino_acids
-	glucose = compartment.glucose
-	water = compartment.water
-	bicarbonate = compartment.bicarbonate
-	protons = compartment.protons
+	_initialize_concentrations()
 
-	actual_sodium = compartment.actual_sodium
-	actual_potassium = compartment.actual_potassium
-	actual_chloride = compartment.actual_chloride
-	actual_amino_acids = compartment.actual_amino_acids
-	actual_glucose = compartment.actual_glucose
-	actual_water = compartment.actual_water
-	actual_bicarbonate = compartment.actual_bicarbonate
-	actual_protons = compartment.actual_protons
+func _initialize_concentrations():
+	# Physiologically accurate blood plasma concentrations (extracellular fluid)
+	set_concentration("sodium", 140.0)     # 140 mM - plasma Na+
+	set_concentration("potassium", 5.0)    # 5 mM - plasma K+
+	set_concentration("chloride", 110.0)   # 110 mM - plasma Cl-
+	set_concentration("glucose", 5.0)      # 5 mM - plasma glucose
+	set_concentration("bicarbonate", 24.0) # 24 mM - plasma HCO3-
+	set_concentration("protons", 0.00004)  # 40 nM (pH 7.4, slightly more basic than cell)
+	set_concentration("amino_acids", 2.5)  # 2.5 mM - plasma amino acids
 
 	concentrations_updated.emit()
+
+func set_concentration(ion_name: String, concentration_mM: float):
+	var actual_count = concentration_mM * 1e-3 * volume * 6.022e23
+	var scale = debug_scale_factor if debug_mode else 1.0
+	var display_count = int(actual_count * scale)
+
+	match ion_name.to_lower():
+		"sodium", "na":
+			actual_sodium = actual_count
+			sodium = display_count
+		"potassium", "k":
+			actual_potassium = actual_count
+			potassium = display_count
+		"chloride", "cl":
+			actual_chloride = actual_count
+			chloride = display_count
+		"glucose":
+			actual_glucose = actual_count
+			glucose = display_count
+		"bicarbonate", "hco3":
+			actual_bicarbonate = actual_count
+			bicarbonate = display_count
+		"protons", "h":
+			actual_protons = actual_count
+			protons = display_count
+		"amino_acids":
+			actual_amino_acids = actual_count
+			amino_acids = display_count
+		"water", "h2o":
+			actual_water = actual_count
+			water = display_count
